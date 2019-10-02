92e224d364605546b0b6b6f54e16a07549483374
melivalen <melissa.gallego@mariaelvinia.edu.co>
2019-10-02 12:26:02 -0500

    Initial commit

---
+++ src/casadeeventos/CasaDeEventos.java
@@ -0,0 +1,19 @@
+
+package casadeeventos;
+
+import java.util.Date; 
+
+
+
+
+public class CasaDeEventos {
+
+    private int numeroDeCajas;
+    private int numeroDeTrabajadores; 
+    private Date fechaDeApertura;
+    private String nombreDelDueño;
+    private boolean tieneSucursales; 
+    
+    
+    
+}
