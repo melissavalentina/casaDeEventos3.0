b6562273dc52807601e1b20e7142c68c204ee4aa
melivalen <melissa.gallego@mariaelvinia.edu.co>
2019-10-02 13:05:29 -0500

    version 3.0

--- src/casadeeventos/CasaDeEventos.java
+++ src/casadeeventos/CasaDeEventos.java
@@ -13,7 +13,54 @@ public class CasaDeEventos {
     private Date fechaDeApertura;
     private String nombreDelDueño;
     private boolean tieneSucursales; 
-    
+
+    public CasaDeEventos(int numeroDeCajas, int numeroDeTrabajadores, Date fechaDeApertura, String nombreDelDueño, boolean tieneSucursales) {
+        this.numeroDeCajas = numeroDeCajas;
+        this.numeroDeTrabajadores = numeroDeTrabajadores;
+        this.fechaDeApertura = fechaDeApertura;
+        this.nombreDelDueño = nombreDelDueño;
+        this.tieneSucursales = tieneSucursales;
+    }
+
+    public int getNumeroDeCajas() {
+        return numeroDeCajas;
+    }
+
+    public int getNumeroDeTrabajadores() {
+        return numeroDeTrabajadores;
+    }
+
+    public Date getFechaDeApertura() {
+        return fechaDeApertura;
+    }
+
+    public String getNombreDelDueño() {
+        return nombreDelDueño;
+    }
+
+    public boolean isTieneSucursales() {
+        return tieneSucursales;
+    }
+
+    public void setNumeroDeCajas(int numeroDeCajas) {
+        this.numeroDeCajas = numeroDeCajas;
+    }
+
+    public void setNumeroDeTrabajadores(int numeroDeTrabajadores) {
+        this.numeroDeTrabajadores = numeroDeTrabajadores;
+    }
+
+    public void setFechaDeApertura(Date fechaDeApertura) {
+        this.fechaDeApertura = fechaDeApertura;
+    }
+
+    public void setNombreDelDueño(String nombreDelDueño) {
+        this.nombreDelDueño = nombreDelDueño;
+    }
+
+    public void setTieneSucursales(boolean tieneSucursales) {
+        this.tieneSucursales = tieneSucursales;
+    }
     
     
 }
